03f3080569d3dcfaa02712efc5b80afa7391cadf
atkvs <atkvs@skbkontur.ru>
2024-09-04 22:57:45 +0500

    add side page (part 1.3) (add pixijs)

--- src/App.tsx
+++ src/App.tsx
@@ -2,8 +2,19 @@ import { Button, SidePage, Toast } from "@skbkontur/react-ui"
 import './App.css';
 import { Editor, useMonaco } from "@monaco-editor/react";
 import { useEffect, useState } from "react";
+import { Stage, Sprite, Container } from '@pixi/react';
+import { KonturColors } from "@skbkontur/colors";
+import kisikIcon from './pinpng.com-cat-png-607501.png';
+import bagIcon from './pinpng.com-bug-png-1058896.png';
 
-const INITIAL_VALUE = "// Опиши действия котика тут";
+const INITIAL_VALUE = `// Опиши действия котика тут
+// За ход котик может сделать только три команды
+// Остальные команды котик проигнорирует (выполнит в следующем спринте)
+// Пример команд на первом ходу
+направо();
+вниз();
+направо();
+`;
 const DEFAULT_LANGUAGE = "myLang";
 
 export const App = () => {
@@ -59,17 +70,26 @@ export const App = () => {
   };
 
   return (
-    <main>
-      <SidePage width={700}>
+    <>
+      <main>
+      <Stage width={1200} height={800} options={{ background: KonturColors.greenMint70 }}>
+      <Container position={[300, 300]}>
+        <Sprite width={130} height={120} image={kisikIcon} x={250} y={220} />
+        <Sprite width={40} height={40} image={bagIcon} x={30} y={10} />
+      </Container>
+    </Stage>
+      </main>
+      <aside>
+      <SidePage width={600}>
         <SidePage.Header>
           Правила игры
         </SidePage.Header>
         <SidePage.Body className="body">
           <p className="rules">
-            Тебе нужно помочь котику с помощью команд поймать все баги.
+            Тебе нужно помочь котику с помощью команд поймать все баги 😸
           </p>
           <p className="rules">
-            Баги будут гулять по коду и временами пропадать сами собой 😅
+            Баги будут гулять по разным частям системы и временами пропадать сами собой 😅
           </p>
           <ul>
             <li>
@@ -109,6 +129,7 @@ export const App = () => {
           </Button>
         </SidePage.Footer>
       </SidePage>
-    </main>
+    </aside>
+    </>
   )
 }
